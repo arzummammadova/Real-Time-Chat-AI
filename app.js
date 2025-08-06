f9aa15ebd6cd362dc1956fe833d824249cfbe3b7
arzummammadova <arzuam-af106@code.edu.az>
2025-08-06 17:10:23 +0400

    first

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import 'dotenv/config'
+import './src/db/connectDB.js'
+const app=express();
+const PORT=process.env.PORT || 5000;
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send('Hello World! ')
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running http://localhost:${PORT}`);
+
+})
